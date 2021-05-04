796c4a7e3fe7eb7f4ef1c67220b2b2db8b9d624b
codeplatoon2 <hartnerk@gmail.com>
2021-05-03 19:09:40 -0700

    Login Hooked up to Django OATH backend

--- src/views/auth/LoginPage.js
+++ src/views/auth/LoginPage.js
@@ -6,12 +6,12 @@ import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
 
 
-function LoginPage () {
+function LoginPage ({history}) {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errors, setErrors] = useState(false)
   const [loading, setLoading] = useState(true)
-
+    
   useEffect(() => {
     if (localStorage.getItem('token') !== null) {
       window.location.replace('http://localhost:3000/dashboard')
@@ -22,39 +22,49 @@ function LoginPage () {
 
   async function onSubmit(e) {
     e.preventDefault()
-
     const user = {
-      email: email,
-      password: password
+      username: email,
+      password: password,
+      grant_type: "password",
+      client_id:'qpSs61fXHZJn6Z5VsY3lBTeTSe79DNiLoQHKQxak',
+      client_secret:'7pOsDVjFYCqRMLNoWTTHBEkE5OixPOwYEeCdt2ztIM1xsvE7iZLSmCl1xsaEHLdgIsmJ5TY5YckjHumQnMiLiIX3HPvIRoE9i98mMOUGLywZd6IWblWbpAu2bwLcgvYD'
     }
 
     try {
+        console.log(user)
+        var formBody = [];
+        for (var property in user) {
+          var encodedKey = encodeURIComponent(property);
+          var encodedValue = encodeURIComponent(user[property]);
+          formBody.push(encodedKey + "=" + encodedValue);
+        }
+        formBody = formBody.join("&");
         let init = {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
+              'Content-Type': 'application/x-www-form-urlencoded'
+            }, 
+            body: formBody
           }
           console.log("FETCH USER LOGIN CALLED")
-          let response = await fetch("http://localhost:3000/v1/users/auth/login/", init)
-          if(response.key){
+          let response = await  fetch("http://localhost:8000/auth/token/", init)
+          console.log('this is your response', response)
+          let data = await response.json();
+          console.log('this is your data', data)
+          if(data.access_token){
             localStorage.clear();
-            localStorage.setItem('token', response.key);
-            window.location.replace('http://localhost:3000/user-profile')
+            localStorage.setItem('access_token', data.access_token);
+            localStorage.setItem('refresh_token', data.refresh_token);
           } else {
             setEmail('')
             setPassword('')
             localStorage.clear()
             setErrors(true)
-          }
-
-        
+          }    
     } catch (error) {
         alert(error)
     }
-
-
+    return history.push('/user-profile')
   }
 
   return (
@@ -68,7 +78,7 @@ function LoginPage () {
                     <Form.Label htmlFor='email'>Email address:</Form.Label>
                     <Form.Control
                         name='email'
-                        type='email'
+                        type='text'
                         value={email}
                         required
                         onChange={e => setEmail(e.target.value)}
