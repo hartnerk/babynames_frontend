ceb70413c1363acc5c2e4eaaa1e6ba882549ae4d
jcrawfo4 <jkbcrawford@gmail.com>
2021-05-05 23:19:57 -0500

    not loading, just errors

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import NameDetailsPage from './views/app/NameDetailsPage';
 import SignupPage from './views/auth/SignupPage';
 import SwiperPage from './views/app/SwiperPage/SwiperPage';
 import MatchPage from './views/app/MatchPage'
-
+import LikedNamePage from './views/app/LikedNamePage'
 function App() {
   return (
     <div className="App">
@@ -28,6 +28,7 @@ function App() {
           <Route path='/signup' component={SignupPage} />
           <Route path='/swiper' component={SwiperPage} />
           <Route path='/matchpage' component={MatchPage} />
+          <Route  path='/liked-names' component={LikedNamePage}/>
         </Switch>
       </Router>
     </div>
--- src/views/app/MatchPage.js
+++ src/views/app/MatchPage.js
@@ -1,50 +1,235 @@
-import React, {useState, useEffect}  from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
 
+// COMPONENTS
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
-const MatchPage = ({ isLoggedIn, user, handleLogout }) => {
-  //const [matchList, setMatchList] = useState()
+// STYLESHEETS
+import { ListGroup } from 'react-bootstrap'
 
-  async function getMatchName(){
+const MatchedNamePage = () => {
+  const [loading, setLoading] = useState(true)
+  const [matchedNames, setMatchedNames] = useState([])
+
+  const fetchMatchedNameFromID = async (nameID) => {
     try {
-      let init = {
-        method:"GET",
-        header:{
-          "Content-Type": "Application/JSON",
-          "Authorization": `Bearer ${localStorage.getItem("access_token")}`
-        }
-      }
-      console.log("matched list is called !!!! ")
-      // let response = await fetch('http://localhost:8000/users/..couples.../coupleID')
-      // setMatchList(response)
+      const response = await fetch(`http://localhost:8000/users/baby-names/${nameID}/`)
+      const data = await response.json()
+      return data
     } catch (error) {
       alert(error)
-      
     }
-
   }
-  useEffect(() => {
-    if (localStorage.getItem('access_token') === null) {
-      window.location.replace('http://localhost:3000/login');
-    } else {
-      getMatchName() 
 
+  const fetchMatchedNameObjs = async (userID) => {
+    try {
+      const response = await fetch('http://localhost:8000/users/couples/1/matched-names/?matched=True')
+      const data = await response.json()
+      await Promise.all(data.map(async(name) => {
+        const baby_name = await(fetchMatchedNameFromID(name.name_id))
+        name['baby_name'] = baby_name.baby_name
+      }))
+      setMatchedNames(data)
+      setLoading(false)
+    } catch (error) {
+      alert(error)
     }
-  }, []);
+  }
+  //console.log( baby_name)
+  console.log(matchedNames, "!!!!!!!!matchedNames!!!!!!")
+  // ** Hard coded couple ID assuming state/props is being passed from somewhere else in app
+  useEffect(() => {
+    fetchMatchedNameObjs(1)
+  }, [])
 
-  let matchList = [ {"usercouple_id":usercouple_id, "name_id":name_id, "matched": matched}]
 
-  return (
-    <div>
-      Couple Match Page
+  const handleOnDragEnd = (result) => {
+    if(!result.destination) return
+    const items = Array.from(matchedNames)
+    const [reorderedItem] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItem)
+    setMatchedNames(items)
+  }
+
+  if(loading) {
+    return (
+      <h1>loading</h1>
+    )
+  } else if(matchedNames.length < 1) {
+    return (
+      <h1>You don't have any MATCHED names yet.</h1>
+    )
+  } else {
+    return(
+      <div>
+        <h1>Your Matched  Baby Names</h1>
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId='names'>
+            {(provided) => (
+            <ListGroup {...provided.droppableProps} ref={provided.innerRef}>
+                {matchedNames.map((name, index) => {
+                  return (
+                    <Draggable key={index} index={index} draggableId={index.toString()}>
+                      {(provided) => (
+                        <ListGroup.Item 
+                          {...provided.draggableProps} 
+                          {...provided.dragHandleProps} 
+                          ref={provided.innerRef}
+                        >
+                          {index+1}: {name.baby_name}
+                        </ListGroup.Item>
+                      )
+                      }
+                    </Draggable>
+                  ) 
+                })}
+              {provided.placeholder}
+            </ListGroup>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    )
+  }
+}
+
+export default MatchedNamePage
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState, useEffect}  from 'react';
+// import { Link } from 'react-router-dom';
+// import LikedNamePage from './LikedNamePage';
+
+// const MatchPage = ({ isLoggedIn, handleLogout }) => {
+//   const [matchList, setMatchList] = useState()
+
+//   async function getMatchName(){
+//     try {
+//       let init = {
+//         method:"GET",
+//         header:{
+//           "Content-Type": "Application/JSON",
+//           "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+//         }
+//       }
+//       console.log("matched list is called !!!! ")
+//       // let response = await fetch('http://localhost:8000/users/couples/coupleID')
+//       // setMatchList(response)
+//       let response = await fetch('http://localhost:8000/users/couples/1/liked-names/?matched=True')
+//       console.log(response.json())
+//     } catch (error) {
+//       alert(error)
+//     }
+
+//   }
+//   useEffect(() => {
+//     if (localStorage.getItem('access_token') === null) {
+//       window.location.replace('http://localhost:3000/login');
+//     } else {
+//       getMatchName() 
+
+//     }
+//   }, []);
+
+//   //let matchList = [ {"usercouple_id":usercouple_id, "name_id":name_id, "matched": matched}]
+
+//   return (
+//     <div>
+//       Couple Match Page
       
-      {
-        matchList.map((item) => <div> {item.name}</div>)
-      }
-    </div>
-  );
-};
+//       {
+//         <div> This will be a list of matches  </div>//matchList.map((item) => <div> {item.name}</div>)
+//       }
+//     </div>
+//   );
+// };
 
-export default MatchPage;
-
-// I was planning to set it up as an api call that would return a list of matches. so you could hardcode something like matches=[1,3,4] and the numbers would be the name id's. Does that make sense?
+// export default MatchPage;
