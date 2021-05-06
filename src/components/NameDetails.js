0060e937a595f91572d9290bdf127c7181a4babb
ja-br <letstalk@jamesbrooks.tech>
2021-05-06 15:10:06 -0400

    Related names and celebrity names are pulled and displayed in component

---
+++ src/components/NameDetails.js
@@ -0,0 +1,75 @@
+import React, {useEffect} from 'react'
+
+
+// COMPONENTS
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+import { useState } from 'react'
+
+
+
+function NameDetailsPage() {
+    const [relatedNames, setRelatedNames] = useState()
+    const [celebrityNames, setCelebrityNames] = useState()
+    const name = "Michael"
+
+    async function getRelatedNames(){
+        const response = await fetch(`https://www.behindthename.com/api/related.json?name=${name}&usage=eng&key=ja675945445`)
+        const data = await response.json()
+        console.log("Related names: ", data.names)
+        setRelatedNames(data.names)
+    }
+
+    async function getCelebrityNames(){
+        let init ={
+            method: 'GET',
+            headers: {
+                "X-Api-Key": "RfT+DAREPh78QFxQa5FQng==yZoIzL6rB6goenMl"
+            }
+        }
+        const response = await fetch(`https://api.celebrityninjas.com/v1/search?limit=10&name=${name}`, init)
+        const data = await response.json()
+        console.log("Celebrity names: ",data)
+        setCelebrityNames(data)
+    }
+
+    useEffect(() => {
+        getRelatedNames()
+        getCelebrityNames()
+       
+    }, [])
+
+    return (
+        <Container>
+            <Card>
+                <Card.Body>
+
+
+                <ListGroup variant="flush">
+                <ListGroup.Item>
+                    <Card.Title>
+                        {name}
+                    </Card.Title>
+
+                    <Card.Text>
+                        Lorem Ipsum
+                    </Card.Text>
+                    </ListGroup.Item>
+                        <ListGroup.Item>
+                            <strong>Related Names: </strong>
+                        <div>
+                            {relatedNames && relatedNames.map((item) => <span> {item} </span>)}
+                        </div> 
+                        </ListGroup.Item>
+                        <ListGroup.Item><strong>Celebrities with the name {name}:</strong>
+                            {celebrityNames && celebrityNames.map((item) => <span> {item.name} </span>)}
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Card.Body>
+            </Card>
+        </Container>
+    )
+}
+
+export default NameDetailsPage
--- src/views/app/PreferencesPage/PreferencesPage.js
+++ src/views/app/PreferencesPage/PreferencesPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Container from 'react-bootstrap/Container'
