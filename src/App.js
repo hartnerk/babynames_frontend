a03c1eeab841a53f2a00d552afe9ba9aded3051d
Caitlin <drkphnx137@gmail.com>
2021-05-07 11:13:57 -0400

    started theme and style folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
+import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 // CONTEXTS
-import { ProfileProvider } from './contexts/ProfileContext';
+import { ProfileProvider } from './contexts/ProfileContext'
 
 
 // COMPONENTS
@@ -10,37 +10,41 @@ import TopNav from './components/TopNav'
 
 // STYLESHEETS
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './styles/Theme'
 
 // VIEWS
 import LoginPage from './views/auth/LoginPage'
 import UserProfilePage from './views/app/UserProfilePage'
-import NameDetails from './components/NameDetails';
-import SignupPage from './views/auth/SignupPage';
-import SwiperPage from './views/app/SwiperPage/SwiperPage';
-import PreferencesPage from './views/app/PreferencesPage/PreferencesPage';
-import MatchPage from './views/app/MatchPage';
-import LikedNamesPage from './views/app/LikedNamesPage';
+import NameDetails from './components/NameDetails'
+import SignupPage from './views/auth/SignupPage'
+import SwiperPage from './views/app/SwiperPage/SwiperPage'
+import PreferencesPage from './views/app/PreferencesPage/PreferencesPage'
+import MatchPage from './views/app/MatchPage'
+import LikedNamesPage from './views/app/LikedNamesPage'
 
 function App() {
   return (
-    <div className="App">
-      <ProfileProvider>
-        <Router>
-          <TopNav />
-          <Switch>
-            <Route path='/login' component={LoginPage} exact />
-            <Route path='/user-profile' component={UserProfilePage} exact />
-            <Route path='/name-detail' component={NameDetails} exact />
-            <Route path='/signup' component={SignupPage} exact />
-            <Route path='/swiper' component={SwiperPage} exact />
-            <Route path='/preferences' component={PreferencesPage} exact />
-            <Route path='/liked-names' component={LikedNamesPage} exact />
-            <Route path='/matchpage' component={MatchPage} exact />
-          </Switch>
-        </Router>
-      </ProfileProvider>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <ProfileProvider>
+          <Router>
+            <TopNav />
+            <Switch>
+              <Route path='/login' component={LoginPage} exact />
+              <Route path='/user-profile' component={UserProfilePage} exact />
+              <Route path='/name-detail' component={NameDetails} exact />
+              <Route path='/signup' component={SignupPage} exact />
+              <Route path='/swiper' component={SwiperPage} exact />
+              <Route path='/preferences' component={PreferencesPage} exact />
+              <Route path='/liked-names' component={LikedNamesPage} exact />
+              <Route path='/matchpage' component={MatchPage} exact />
+            </Switch>
+          </Router>
+        </ProfileProvider>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ src/styles/Theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  darkest: '#AC598D', //purple
+  darker: '#C96984', //dark pink
+  dark: '#E57778', //dark orange
+  light: '#E57778', //orange
+  lighter: '#E57778', //light orange
+  lightest: '#ECCE8D' //yellow
+}
